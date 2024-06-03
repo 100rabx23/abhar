22ac012c165c0ed33ec11cf48a681d0c0eb62133
100RAB <115261525+100rabx23@users.noreply.github.com>
2024-06-03 20:55:34 +0530

    Update cmd_ps.bat

--- bin/cmd_ps.bat
+++ bin/cmd_ps.bat
@@ -3,3 +3,4 @@ rem safe bet
 call "%~dp0env_for_icons.bat"
 Powershell.exe -Command "& {Start-Process PowerShell.exe -ArgumentList '-ExecutionPolicy RemoteSigned -noexit -File ""%~dp0WinPython_PS_Prompt.ps1""'}"
 exit
+#$
